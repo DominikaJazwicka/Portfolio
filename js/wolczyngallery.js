0a8ab23fcae7f8481c18c702c6ddb74ec5625a77
DominikaJazwicka <109682344+DominikaJazwicka@users.noreply.github.com>
2022-12-14 23:26:57 +0100

    main

--- js/wolczyngallery.js
+++ js/wolczyngallery.js
@@ -1,5 +1,17 @@
-const wolczyn__element = document.querySelectorAll("wolczyn__element img");
+const GALLERY__ELEMENT = document.querySelectorAll(".gallery__element img");
+const POPUP = document.querySelector(".popup");
+const POPUP__CLOSE = document.querySelector(".popup__close");
+const POPUP__IMG = document.querySelector(".popup__img");
 
-wolczyn__element.forEach((wolczyn__element) => {
-    wolczyn__element.addEventListener("click", () => console.log(wolczyn__element));
+
+GALLERY__ELEMENT.forEach((gallery__element) => {
+    gallery__element.addEventListener("click", (e) => {
+    POPUP.classList.remove("hidden");
+    console.log(e.target);
 });
+});
+
+POPUP__CLOSE.addEventListener("click", () => {
+    POPUP.classList.add("hidden");
+});
+
