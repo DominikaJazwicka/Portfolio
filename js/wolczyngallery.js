1bdd4ab22a009354edab156d5540fff8257323f7
DominikaJazwicka <109682344+DominikaJazwicka@users.noreply.github.com>
2022-12-15 00:18:22 +0100

    main

--- js/wolczyngallery.js
+++ js/wolczyngallery.js
@@ -2,12 +2,15 @@ const GALLERY__ELEMENT = document.querySelectorAll(".gallery__element img");
 const POPUP = document.querySelector(".popup");
 const POPUP__CLOSE = document.querySelector(".popup__close");
 const POPUP__IMG = document.querySelector(".popup__img");
+const ARROW__LEFT  = document.querySelector(".popup__arrow--left");
+const ARROW__RIGHT = document.querySelector(".popup__arrow--right");
 
 
-GALLERY__ELEMENT.forEach((gallery__element) => {
+GALLERY__ELEMENT.forEach((gallery__element, index) => {
     gallery__element.addEventListener("click", (e) => {
     POPUP.classList.remove("hidden");
     POPUP__IMG.src = e.target.src;
+    currentImgIndex = index
 });
 });
 
@@ -15,3 +18,21 @@ POPUP__CLOSE.addEventListener("click", () => {
     POPUP.classList.add("hidden");
 });
 
+ARROW__RIGHT.addEventListener("click", () => {
+    if (currentImgIndex === GALLERY__ELEMENT.length - 1) {
+        currentImgIndex = 0;
+    } else {
+        currentImgIndex++;
+    }
+    POPUP__IMG.src = GALLERY__ELEMENT[currentImgIndex].src;
+});
+
+
+ARROW__LEFT.addEventListener("click", () => {
+    if (currentImgIndex === 0) {
+        currentImgIndex = GALLERY__ELEMENT.length - 1;
+    } else {
+        currentImgIndex--;
+    }
+    POPUP__IMG.src = GALLERY__ELEMENT[currentImgIndex].src;
+})
